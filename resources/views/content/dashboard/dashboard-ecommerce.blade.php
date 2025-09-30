ab88d78461d07133fcdb5d6bf9dd67d905d67e7a
Nedim Kulašin <kulasinn@gmail.com>
2025-09-30 11:22:13 +0200

    test-commit

--- resources/views/content/dashboard/dashboard-ecommerce.blade.php
+++ resources/views/content/dashboard/dashboard-ecommerce.blade.php
@@ -23,7 +23,7 @@
     <div class="col-xl-4 col-md-6 col-12">
       <div class="card card-congratulation-medal">
         <div class="card-body">
-          <h5>Congratulations 🎉 John!</h5>
+          <h5>Congratulations 🎉 Nedim!</h5>
           <p class="card-text font-small-3">You have won gold medal</p>
           <h3 class="mb-75 mt-2 pt-50">
             <a href="#">$48.9k</a>
