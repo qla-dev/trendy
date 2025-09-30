da264877ab5f6ba14a1ea48fb5360c408c9764c2
Nedim Kulašin <kulasinn@gmail.com>
2025-09-30 22:45:25 +0200

    first-wave-commit

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -54,37 +54,17 @@
             data-feather="menu"></i></a></li>
     </ul>
     <ul class="nav navbar-nav bookmark-icons">
-      <li class="nav-item d-none d-lg-block"><a class="nav-link" href="{{ url('app/email') }}"
-          data-bs-toggle="tooltip" data-bs-placement="bottom" title="Email"><i class="ficon"
-            data-feather="mail"></i></a></li>
-      <li class="nav-item d-none d-lg-block"><a class="nav-link" href="{{ url('app/chat') }}"
-          data-bs-toggle="tooltip" data-bs-placement="bottom" title="Chat"><i class="ficon"
-            data-feather="message-square"></i></a></li>
-      <li class="nav-item d-none d-lg-block"><a class="nav-link" href="{{ url('app/calendar') }}"
-          data-bs-toggle="tooltip" data-bs-placement="bottom" title="Calendar"><i class="ficon"
+      <li class="nav-item d-none d-md-block"><a class="nav-link" href="{{ url('app/calendar') }}"
+          data-bs-toggle="tooltip" data-bs-placement="bottom" title="Kalendar"><i class="ficon"
             data-feather="calendar"></i></a></li>
-      <li class="nav-item d-none d-lg-block"><a class="nav-link" href="{{ url('app/todo') }}"
-          data-bs-toggle="tooltip" data-bs-placement="bottom" title="Todo"><i class="ficon"
-            data-feather="check-square"></i></a></li>
-    </ul>
-    <ul class="nav navbar-nav">
-      <li class="nav-item d-none d-lg-block">
-        <a class="nav-link bookmark-star">
-          <i class="ficon text-warning" data-feather="star"></i>
-        </a>
-        <div class="bookmark-input search-input">
-          <div class="bookmark-input-icon">
-            <i data-feather="search"></i>
-          </div>
-          <input class="form-control input" type="text" placeholder="Bookmark" tabindex="0" data-search="search">
-          <ul class="search-list search-list-bookmark"></ul>
-        </div>
-      </li>
+      <li class="nav-item d-none d-md-block"><a class="nav-link" href="{{ url('app/invoice/preview') }}"
+          data-bs-toggle="tooltip" data-bs-placement="bottom" title="Skeniraj nalog"><i class="ficon"
+            data-feather="camera"></i></a></li>
     </ul>
   </div>
   <ul class="nav navbar-nav align-items-center ms-auto">
     
-    <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon"
+    <li class="nav-item d-none d-md-block"><a class="nav-link nav-link-style"><i class="ficon"
           data-feather="{{ $configData['theme'] === 'dark' ? 'sun' : 'moon' }}"></i></a></li>
     <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon"
           data-feather="search"></i></a>
@@ -94,222 +74,6 @@
         <div class="search-input-close"><i data-feather="x"></i></div>
         <ul class="search-list search-list-main"></ul>
       </div>
-    </li>
-    <li class="nav-item dropdown dropdown-cart me-25">
-      <a class="nav-link" href="javascript:void(0);" data-bs-toggle="dropdown">
-        <i class="ficon" data-feather="shopping-cart"></i>
-        <span class="badge rounded-pill bg-primary badge-up cart-item-count">6</span>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-media dropdown-menu-end">
-        <li class="dropdown-menu-header">
-          <div class="dropdown-header d-flex">
-            <h4 class="notification-title mb-0 me-auto">My Cart</h4>
-            <div class="badge rounded-pill badge-light-primary">4 Items</div>
-          </div>
-        </li>
-        <li class="scrollable-container media-list">
-          <div class="list-item align-items-center">
-            <img class="d-block rounded me-1" src="{{ asset('images/pages/eCommerce/1.png') }}" alt="donuts"
-              width="62">
-            <div class="list-item-body flex-grow-1">
-              <i class="ficon cart-item-remove" data-feather="x"></i>
-              <div class="media-heading">
-                <h6 class="cart-item-title"><a class="text-body" href="{{ url('app/ecommerce/details') }}">
-                    Apple
-                    watch 5</a></h6><small class="cart-item-by">By Apple</small>
-              </div>
-              <div class="cart-item-qty">
-                <div class="input-group">
-                  <input class="touchspin-cart" type="number" value="1">
-                </div>
-              </div>
-              <h5 class="cart-item-price">$374.90</h5>
-            </div>
-          </div>
-          <div class="list-item align-items-center">
-            <img class="d-block rounded me-1" src="{{ asset('images/pages/eCommerce/7.png') }}" alt="donuts"
-              width="62">
-            <div class="list-item-body flex-grow-1">
-              <i class="ficon cart-item-remove" data-feather="x"></i>
-              <div class="media-heading">
-                <h6 class="cart-item-title"><a class="text-body" href="{{ url('app/ecommerce/details') }}">
-                    Google
-                    Home Mini</a></h6><small class="cart-item-by">By Google</small>
-              </div>
-              <div class="cart-item-qty">
-                <div class="input-group">
-                  <input class="touchspin-cart" type="number" value="3">
-                </div>
-              </div>
-              <h5 class="cart-item-price">$129.40</h5>
-            </div>
-          </div>
-          <div class="list-item align-items-center">
-            <img class="d-block rounded me-1" src="{{ asset('images/pages/eCommerce/2.png') }}" alt="donuts"
-              width="62">
-            <div class="list-item-body flex-grow-1">
-              <i class="ficon cart-item-remove" data-feather="x"></i>
-              <div class="media-heading">
-                <h6 class="cart-item-title"><a class="text-body" href="{{ url('app/ecommerce/details') }}">
-                    iPhone 11 Pro</a></h6><small class="cart-item-by">By Apple</small>
-              </div>
-              <div class="cart-item-qty">
-                <div class="input-group">
-                  <input class="touchspin-cart" type="number" value="2">
-                </div>
-              </div>
-              <h5 class="cart-item-price">$699.00</h5>
-            </div>
-          </div>
-          <div class="list-item align-items-center">
-            <img class="d-block rounded me-1" src="{{ asset('images/pages/eCommerce/3.png') }}" alt="donuts"
-              width="62">
-            <div class="list-item-body flex-grow-1">
-              <i class="ficon cart-item-remove" data-feather="x"></i>
-              <div class="media-heading">
-                <h6 class="cart-item-title"><a class="text-body" href="{{ url('app/ecommerce/details') }}">
-                    iMac
-                    Pro</a></h6><small class="cart-item-by">By Apple</small>
-              </div>
-              <div class="cart-item-qty">
-                <div class="input-group">
-                  <input class="touchspin-cart" type="number" value="1">
-                </div>
-              </div>
-              <h5 class="cart-item-price">$4,999.00</h5>
-            </div>
-          </div>
-          <div class="list-item align-items-center">
-            <img class="d-block rounded me-1" src="{{ asset('images/pages/eCommerce/5.png') }}" alt="donuts"
-              width="62">
-            <div class="list-item-body flex-grow-1">
-              <i class="ficon cart-item-remove" data-feather="x"></i>
-              <div class="media-heading">
-                <h6 class="cart-item-title"><a class="text-body" href="{{ url('app/ecommerce/details') }}">
-                    MacBook Pro</a></h6><small class="cart-item-by">By Apple</small>
-              </div>
-              <div class="cart-item-qty">
-                <div class="input-group">
-                  <input class="touchspin-cart" type="number" value="1">
-                </div>
-              </div>
-              <h5 class="cart-item-price">$2,999.00</h5>
-            </div>
-          </div>
-        </li>
-        <li class="dropdown-menu-footer">
-          <div class="d-flex justify-content-between mb-1">
-            <h6 class="fw-bolder mb-0">Total:</h6>
-            <h6 class="text-primary fw-bolder mb-0">$10,999.00</h6>
-          </div>
-          <a class="btn btn-primary w-100" href="{{ url('app/ecommerce/checkout') }}">Checkout</a>
-        </li>
-      </ul>
-    </li>
-    <li class="nav-item dropdown dropdown-notification me-25">
-      <a class="nav-link" href="javascript:void(0);" data-bs-toggle="dropdown">
-        <i class="ficon" data-feather="bell"></i>
-        <span class="badge rounded-pill bg-danger badge-up">5</span>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-media dropdown-menu-end">
-        <li class="dropdown-menu-header">
-          <div class="dropdown-header d-flex">
-            <h4 class="notification-title mb-0 me-auto">Notifications</h4>
-            <div class="badge rounded-pill badge-light-primary">6 New</div>
-          </div>
-        </li>
-        <li class="scrollable-container media-list">
-          <a class="d-flex" href="javascript:void(0)">
-            <div class="list-item d-flex align-items-start">
-              <div class="me-1">
-                <div class="avatar">
-                  <img src="{{ asset('images/portrait/small/avatar-s-15.jpg') }}" alt="avatar" width="32"
-                    height="32">
-                </div>
-              </div>
-              <div class="list-item-body flex-grow-1">
-                <p class="media-heading"><span class="fw-bolder">Congratulation Sam 🎉</span>winner!</p>
-                <small class="notification-text"> Won the monthly best seller badge.</small>
-              </div>
-            </div>
-          </a>
-          <a class="d-flex" href="javascript:void(0)">
-            <div class="list-item d-flex align-items-start">
-              <div class="me-1">
-                <div class="avatar">
-                  <img src="{{ asset('images/portrait/small/avatar-s-3.jpg') }}" alt="avatar" width="32" height="32">
-                </div>
-              </div>
-              <div class="list-item-body flex-grow-1">
-                <p class="media-heading"><span class="fw-bolder">New message</span>&nbsp;received</p>
-                <small class="notification-text"> You have 10 unread messages</small>
-              </div>
-            </div>
-          </a>
-          <a class="d-flex" href="javascript:void(0)">
-            <div class="list-item d-flex align-items-start">
-              <div class="me-1">
-                <div class="avatar bg-light-danger">
-                  <div class="avatar-content">MD</div>
-                </div>
-              </div>
-              <div class="list-item-body flex-grow-1">
-                <p class="media-heading"><span class="fw-bolder">Revised Order 👋</span>&nbsp;checkout</p>
-                <small class="notification-text"> MD Inc. order updated</small>
-              </div>
-            </div>
-          </a>
-          <div class="list-item d-flex align-items-center">
-            <h6 class="fw-bolder me-auto mb-0">System Notifications</h6>
-            <div class="form-check form-check-primary form-switch">
-              <input class="form-check-input" id="systemNotification" type="checkbox" checked="">
-              <label class="form-check-label" for="systemNotification"></label>
-            </div>
-          </div>
-          <a class="d-flex" href="javascript:void(0)">
-            <div class="list-item d-flex align-items-start">
-              <div class="me-1">
-                <div class="avatar bg-light-danger">
-                  <div class="avatar-content"><i class="avatar-icon" data-feather="x"></i></div>
-                </div>
-              </div>
-              <div class="list-item-body flex-grow-1">
-                <p class="media-heading"><span class="fw-bolder">Server down</span>&nbsp;registered</p>
-                <small class="notification-text"> USA Server is down due to hight CPU usage</small>
-              </div>
-            </div>
-          </a>
-          <a class="d-flex" href="javascript:void(0)">
-            <div class="list-item d-flex align-items-start">
-              <div class="me-1">
-                <div class="avatar bg-light-success">
-                  <div class="avatar-content"><i class="avatar-icon" data-feather="check"></i></div>
-                </div>
-              </div>
-              <div class="list-item-body flex-grow-1">
-                <p class="media-heading"><span class="fw-bolder">Sales report</span>&nbsp;generated</p><small
-                  class="notification-text"> Last month sales report generated</small>
-              </div>
-            </div>
-          </a>
-          <a class="d-flex" href="javascript:void(0)">
-            <div class="list-item d-flex align-items-start">
-              <div class="me-1">
-                <div class="avatar bg-light-warning">
-                  <div class="avatar-content"><i class="avatar-icon" data-feather="alert-triangle"></i></div>
-                </div>
-              </div>
-              <div class="list-item-body flex-grow-1">
-                <p class="media-heading"><span class="fw-bolder">High memory</span>&nbsp;usage</p><small
-                  class="notification-text"> BLR Server using high memory</small>
-              </div>
-            </div>
-          </a>
-        </li>
-        <li class="dropdown-menu-footer">
-          <a class="btn btn-primary w-100" href="javascript:void(0)">Read all notifications</a>
-        </li>
-      </ul>
     </li>
     <li class="nav-item dropdown dropdown-user">
       <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="javascript:void(0);"
@@ -323,7 +87,13 @@
             @endif
           </span>
           <span class="user-status">
-            Admin
+          @if(Auth::user()->role === 'admin')
+            ADMIN
+          @elseif(Auth::user()->role === 'user')
+            KORISNIK
+          @else
+            {{ strtoupper(Auth::user()->role ?? '') }}
+          @endif
           </span>
         </div>
         <span class="avatar">
@@ -339,17 +109,6 @@
         <a class="dropdown-item"
           href="{{ Route::has('profile.show') ? route('profile.show') : 'javascript:void(0)' }}">
           <i class="me-50" data-feather="user"></i> Profile
-        </a>
-        @if (Auth::check())
-          <a class="dropdown-item" href="{{ route('auth.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            <i class="me-50" data-feather="log-out"></i> Logout
-          </a>
-          <form id="logout-form" action="{{ route('auth.logout') }}" method="POST" style="display: none;">
-            @csrf
-          </form>
-        @endif
-        <a class="dropdown-item" href="#">
-          <i class="me-50" data-feather="settings"></i> Settings
         </a>
 
         @if (Auth::check())
