aef135dd854bd8748554344cc0b1b1a48fcb0224
Nedim Kulašin <kulasinn@gmail.com>
2025-12-10 23:58:31 +0100

    dashboard

--- resources/views/content/dashboard/dashboard-ecommerce.blade.php
+++ resources/views/content/dashboard/dashboard-ecommerce.blade.php
@@ -20,19 +20,107 @@
 <section id="dashboard-ecommerce">
   <div class="row match-height">
     <!-- Medal Card -->
-    <div class="col-xl-4 col-md-6 col-12">
-      <div class="card card-congratulation-medal">
+      <!-- Developer Meetup Card -->
+    <div class="col-lg-4 col-md-6 col-12">
+      <div class="card card-developer-meetup">
+        <div class="meetup-img-wrapper rounded-top text-center">
+          <img src="{{asset('images/illustration/email.svg')}}" alt="Meeting Pic" height="170" />
+        </div>
         <div class="card-body">
-          <h5>Congratulations 🎉 Nedim!</h5>
-          <p class="card-text font-small-3">You have won gold medal</p>
-          <h3 class="mb-75 mt-2 pt-50">
-            <a href="#">$48.9k</a>
-          </h3>
-          <button type="button" class="btn btn-primary">View Sales</button>
-          <img src="{{asset('images/illustration/badge.svg')}}" class="congratulation-medal" alt="Medal Pic" />
+          <div class="meetup-header d-flex align-items-center">
+            <div class="meetup-day">
+              <h6 class="mb-0">THU</h6>
+              <h3 class="mb-0">24</h3>
+            </div>
+            <div class="my-auto">
+              <h4 class="card-title mb-25">Developer Meetup</h4>
+              <p class="card-text mb-0">Meet world popular developers</p>
+            </div>
+          </div>
+          <div class="mt-0">
+            <div class="avatar float-start bg-light-primary rounded me-1">
+              <div class="avatar-content">
+                <i data-feather="calendar" class="avatar-icon font-medium-3"></i>
+              </div>
+            </div>
+            <div class="more-info">
+              <h6 class="mb-0">Sat, May 25, 2020</h6>
+              <small>10:AM to 6:PM</small>
+            </div>
+          </div>
+          <div class="mt-2">
+            <div class="avatar float-start bg-light-primary rounded me-1">
+              <div class="avatar-content">
+                <i data-feather="map-pin" class="avatar-icon font-medium-3"></i>
+              </div>
+            </div>
+            <div class="more-info">
+              <h6 class="mb-0">Central Park</h6>
+              <small>Manhattan, New york City</small>
+            </div>
+          </div>
+          <div class="avatar-group">
+            <div
+              data-bs-toggle="tooltip"
+              data-popup="tooltip-custom"
+              data-bs-placement="bottom"
+              title="Billy Hopkins"
+              class="avatar pull-up"
+            >
+              <img src="{{asset('images/portrait/small/avatar-s-9.jpg')}}" alt="Avatar" width="33" height="33" />
+            </div>
+            <div
+              data-bs-toggle="tooltip"
+              data-popup="tooltip-custom"
+              data-bs-placement="bottom"
+              title="Amy Carson"
+              class="avatar pull-up"
+            >
+              <img src="{{asset('images/portrait/small/avatar-s-6.jpg')}}" alt="Avatar" width="33" height="33" />
+            </div>
+            <div
+              data-bs-toggle="tooltip"
+              data-popup="tooltip-custom"
+              data-bs-placement="bottom"
+              title="Brandon Miles"
+              class="avatar pull-up"
+            >
+              <img src="{{asset('images/portrait/small/avatar-s-8.jpg')}}" alt="Avatar" width="33" height="33" />
+            </div>
+            <div
+              data-bs-toggle="tooltip"
+              data-popup="tooltip-custom"
+              data-bs-placement="bottom"
+              title="Daisy Weber"
+              class="avatar pull-up"
+            >
+              <img
+                src="{{asset('images/portrait/small/avatar-s-20.jpg')}}"
+                alt="Avatar"
+                width="33"
+                height="33"
+              />
+            </div>
+            <div
+              data-bs-toggle="tooltip"
+              data-popup="tooltip-custom"
+              data-bs-placement="bottom"
+              title="Jenny Looper"
+              class="avatar pull-up"
+            >
+              <img
+                src="{{asset('images/portrait/small/avatar-s-20.jpg')}}"
+                alt="Avatar"
+                width="33"
+                height="33"
+              />
+            </div>
+            <h6 class="align-self-center cursor-pointer ms-50 mb-0">+42</h6>
+          </div>
         </div>
       </div>
     </div>
+    <!--/ Developer Meetup Card -->
     <!--/ Medal Card -->
 
     <!-- Statistics Card -->
@@ -498,200 +586,9 @@
     </div>
     <!--/ Company Table Card -->
 
-    <!-- Developer Meetup Card -->
-    <div class="col-lg-4 col-md-6 col-12">
-      <div class="card card-developer-meetup">
-        <div class="meetup-img-wrapper rounded-top text-center">
-          <img src="{{asset('images/illustration/email.svg')}}" alt="Meeting Pic" height="170" />
-        </div>
-        <div class="card-body">
-          <div class="meetup-header d-flex align-items-center">
-            <div class="meetup-day">
-              <h6 class="mb-0">THU</h6>
-              <h3 class="mb-0">24</h3>
-            </div>
-            <div class="my-auto">
-              <h4 class="card-title mb-25">Developer Meetup</h4>
-              <p class="card-text mb-0">Meet world popular developers</p>
-            </div>
-          </div>
-          <div class="mt-0">
-            <div class="avatar float-start bg-light-primary rounded me-1">
-              <div class="avatar-content">
-                <i data-feather="calendar" class="avatar-icon font-medium-3"></i>
-              </div>
-            </div>
-            <div class="more-info">
-              <h6 class="mb-0">Sat, May 25, 2020</h6>
-              <small>10:AM to 6:PM</small>
-            </div>
-          </div>
-          <div class="mt-2">
-            <div class="avatar float-start bg-light-primary rounded me-1">
-              <div class="avatar-content">
-                <i data-feather="map-pin" class="avatar-icon font-medium-3"></i>
-              </div>
-            </div>
-            <div class="more-info">
-              <h6 class="mb-0">Central Park</h6>
-              <small>Manhattan, New york City</small>
-            </div>
-          </div>
-          <div class="avatar-group">
-            <div
-              data-bs-toggle="tooltip"
-              data-popup="tooltip-custom"
-              data-bs-placement="bottom"
-              title="Billy Hopkins"
-              class="avatar pull-up"
-            >
-              <img src="{{asset('images/portrait/small/avatar-s-9.jpg')}}" alt="Avatar" width="33" height="33" />
-            </div>
-            <div
-              data-bs-toggle="tooltip"
-              data-popup="tooltip-custom"
-              data-bs-placement="bottom"
-              title="Amy Carson"
-              class="avatar pull-up"
-            >
-              <img src="{{asset('images/portrait/small/avatar-s-6.jpg')}}" alt="Avatar" width="33" height="33" />
-            </div>
-            <div
-              data-bs-toggle="tooltip"
-              data-popup="tooltip-custom"
-              data-bs-placement="bottom"
-              title="Brandon Miles"
-              class="avatar pull-up"
-            >
-              <img src="{{asset('images/portrait/small/avatar-s-8.jpg')}}" alt="Avatar" width="33" height="33" />
-            </div>
-            <div
-              data-bs-toggle="tooltip"
-              data-popup="tooltip-custom"
-              data-bs-placement="bottom"
-              title="Daisy Weber"
-              class="avatar pull-up"
-            >
-              <img
-                src="{{asset('images/portrait/small/avatar-s-20.jpg')}}"
-                alt="Avatar"
-                width="33"
-                height="33"
-              />
-            </div>
-            <div
-              data-bs-toggle="tooltip"
-              data-popup="tooltip-custom"
-              data-bs-placement="bottom"
-              title="Jenny Looper"
-              class="avatar pull-up"
-            >
-              <img
-                src="{{asset('images/portrait/small/avatar-s-20.jpg')}}"
-                alt="Avatar"
-                width="33"
-                height="33"
-              />
-            </div>
-            <h6 class="align-self-center cursor-pointer ms-50 mb-0">+42</h6>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--/ Developer Meetup Card -->
+ 
 
-    <!-- Browser States Card -->
-    <div class="col-lg-4 col-md-6 col-12">
-      <div class="card card-browser-states">
-        <div class="card-header">
-          <div>
-            <h4 class="card-title">Browser States</h4>
-            <p class="card-text font-small-2">Counter August 2020</p>
-          </div>
-          <div class="dropdown chart-dropdown">
-            <i data-feather="more-vertical" class="font-medium-3 cursor-pointer" data-bs-toggle="dropdown"></i>
-            <div class="dropdown-menu dropdown-menu-end">
-              <a class="dropdown-item" href="#">Last 28 Days</a>
-              <a class="dropdown-item" href="#">Last Month</a>
-              <a class="dropdown-item" href="#">Last Year</a>
-            </div>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="browser-states">
-            <div class="d-flex">
-              <img
-                src="{{asset('images/icons/google-chrome.png')}}"
-                class="rounded me-1"
-                height="30"
-                alt="Google Chrome"
-              />
-              <h6 class="align-self-center mb-0">Google Chrome</h6>
-            </div>
-            <div class="d-flex align-items-center">
-              <div class="fw-bold text-body-heading me-1">54.4%</div>
-              <div id="browser-state-chart-primary"></div>
-            </div>
-          </div>
-          <div class="browser-states">
-            <div class="d-flex">
-              <img
-                src="{{asset('images/icons/mozila-firefox.png')}}"
-                class="rounded me-1"
-                height="30"
-                alt="Mozila Firefox"
-              />
-              <h6 class="align-self-center mb-0">Mozila Firefox</h6>
-            </div>
-            <div class="d-flex align-items-center">
-              <div class="fw-bold text-body-heading me-1">6.1%</div>
-              <div id="browser-state-chart-warning"></div>
-            </div>
-          </div>
-          <div class="browser-states">
-            <div class="d-flex">
-              <img
-                src="{{asset('images/icons/apple-safari.png')}}"
-                class="rounded me-1"
-                height="30"
-                alt="Apple Safari"
-              />
-              <h6 class="align-self-center mb-0">Apple Safari</h6>
-            </div>
-            <div class="d-flex align-items-center">
-              <div class="fw-bold text-body-heading me-1">14.6%</div>
-              <div id="browser-state-chart-secondary"></div>
-            </div>
-          </div>
-          <div class="browser-states">
-            <div class="d-flex">
-              <img
-                src="{{asset('images/icons/internet-explorer.png')}}"
-                class="rounded me-1"
-                height="30"
-                alt="Internet Explorer"
-              />
-              <h6 class="align-self-center mb-0">Internet Explorer</h6>
-            </div>
-            <div class="d-flex align-items-center">
-              <div class="fw-bold text-body-heading me-1">4.2%</div>
-              <div id="browser-state-chart-info"></div>
-            </div>
-          </div>
-          <div class="browser-states">
-            <div class="d-flex">
-              <img src="{{asset('images/icons/opera.png')}}" class="rounded me-1" height="30" alt="Opera Mini" />
-              <h6 class="align-self-center mb-0">Opera Mini</h6>
-            </div>
-            <div class="d-flex align-items-center">
-              <div class="fw-bold text-body-heading me-1">8.4%</div>
-              <div id="browser-state-chart-danger"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--/ Browser States Card -->
+ 
 
     <!-- Goal Overview Card -->
     <div class="col-lg-4 col-md-6 col-12">
@@ -717,94 +614,7 @@
     </div>
     <!--/ Goal Overview Card -->
 
-    <!-- Transaction Card -->
-    <div class="col-lg-4 col-md-6 col-12">
-      <div class="card card-transaction">
-        <div class="card-header">
-          <h4 class="card-title">Transactions</h4>
-          <div class="dropdown chart-dropdown">
-            <i data-feather="more-vertical" class="font-medium-3 cursor-pointer" data-bs-toggle="dropdown"></i>
-            <div class="dropdown-menu dropdown-menu-end">
-              <a class="dropdown-item" href="#">Last 28 Days</a>
-              <a class="dropdown-item" href="#">Last Month</a>
-              <a class="dropdown-item" href="#">Last Year</a>
-            </div>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="transaction-item">
-            <div class="d-flex">
-              <div class="avatar bg-light-primary rounded float-start">
-                <div class="avatar-content">
-                  <i data-feather="pocket" class="avatar-icon font-medium-3"></i>
-                </div>
-              </div>
-              <div class="transaction-percentage">
-                <h6 class="transaction-title">Wallet</h6>
-                <small>Starbucks</small>
-              </div>
-            </div>
-            <div class="fw-bolder text-danger">- $74</div>
-          </div>
-          <div class="transaction-item">
-            <div class="d-flex">
-              <div class="avatar bg-light-success rounded float-start">
-                <div class="avatar-content">
-                  <i data-feather="check" class="avatar-icon font-medium-3"></i>
-                </div>
-              </div>
-              <div class="transaction-percentage">
-                <h6 class="transaction-title">Bank Transfer</h6>
-                <small>Add Money</small>
-              </div>
-            </div>
-            <div class="fw-bolder text-success">+ $480</div>
-          </div>
-          <div class="transaction-item">
-            <div class="d-flex">
-              <div class="avatar bg-light-danger rounded float-start">
-                <div class="avatar-content">
-                  <i data-feather="dollar-sign" class="avatar-icon font-medium-3"></i>
-                </div>
-              </div>
-              <div class="transaction-percentage">
-                <h6 class="transaction-title">Paypal</h6>
-                <small>Add Money</small>
-              </div>
-            </div>
-            <div class="fw-bolder text-success">+ $590</div>
-          </div>
-          <div class="transaction-item">
-            <div class="d-flex">
-              <div class="avatar bg-light-warning rounded float-start">
-                <div class="avatar-content">
-                  <i data-feather="credit-card" class="avatar-icon font-medium-3"></i>
-                </div>
-              </div>
-              <div class="transaction-percentage">
-                <h6 class="transaction-title">Mastercard</h6>
-                <small>Ordered Food</small>
-              </div>
-            </div>
-            <div class="fw-bolder text-danger">- $23</div>
-          </div>
-          <div class="transaction-item">
-            <div class="d-flex">
-              <div class="avatar bg-light-info rounded float-start">
-                <div class="avatar-content">
-                  <i data-feather="trending-up" class="avatar-icon font-medium-3"></i>
-                </div>
-              </div>
-              <div class="transaction-percentage">
-                <h6 class="transaction-title">Transfer</h6>
-                <small>Refund</small>
-              </div>
-            </div>
-            <div class="fw-bolder text-success">+ $98</div>
-          </div>
-        </div>
-      </div>
-    </div>
+
     <!--/ Transaction Card -->
   </div>
 </section>
